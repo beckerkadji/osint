b06e704cc52f216d0921f8e439b741313e37bdf1
beckerkadji <kadjibecker@gmail.com>
2023-05-14 08:34:15 +0100

    update next js config content security policy

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async headers(){
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' 'http://35.181.28.82:8080' "
-          }
-        ]
-      }
-    ]
-  },
   reactStrictMode: true,
   swcMinify: true,
   experimental:{
