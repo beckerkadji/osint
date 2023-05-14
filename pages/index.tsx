a59f58fd02f298f4fb0203283c2286483c624275
beckerkadji <kadjibecker@gmail.com>
2023-05-14 12:34:42 +0100

    landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <div className="text-3xl">Hello Guys!!!!!!!</div>
+    return (
+        <div className="w-screen h-screen bg-whitecolor flex flex-col items-center"> 
+            <h1 className="uppercase text-blackcolor text-4xl mt-[60px] p-4 font-bold mt-10 mb-10">Welcome to OSINT-GN</h1>
+            <Link href={'/login'}> <button className="bg-blackcolor py-4 px-6 text-whitecolor uppercase">login</button></Link>
+        </div>
+    )
+
 }
