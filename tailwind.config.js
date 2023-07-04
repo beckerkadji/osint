6ae01939d509d919b29ce01f79c27a24e6fa99d8
beckerkadji <kadjibecker@gmail.com>
2023-07-04 15:46:33 +0100

    update tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         './node_modules/tw-elements/dist/js/**/*.js',
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
+        './app/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         screens: {
